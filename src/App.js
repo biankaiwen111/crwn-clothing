621251c9249b9404bf828efa458feddcb33c7972
biankaiwen111 <biankaiwen111@outlook.com>
2022-01-27 22:36:36 -0800

    finishing signin and signup(OAuth and normal username/password)

--- src/App.js
+++ src/App.js
@@ -24,12 +24,17 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
           console.log("found");
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
             },
-          });
+            () => {
+              console.log(this.state);
+            }
+          );
         });
       } else {
         this.setState({ currentUser: null });
